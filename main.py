8e106f17ea115eb02c3bf1c84f3c57c54332e975
jorypettit <jorypettit@Jorys-MacBook-Pro.local>
2016-10-31 15:53:43 -0400

    order of html load

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 class LoginPage(webapp2.RequestHandler):
     def get(self):
-        template = JINJA_ENVIRONMENT.get_template('index.html')
+        template = JINJA_ENVIRONMENT.get_template('loginMQ.html')
         self.response.out.write(template.render())
 
 class Login(webapp2.RequestHandler):
